6114f4134e4bf0c99dd8707b27b3f0d279154885
freakymind12 <riankurniawan132@gmail.com>
2025-07-17 14:03:30 +0700

    fix: update pie chart config

--- src/views/report/MonthlyReport.vue
+++ src/views/report/MonthlyReport.vue
@@ -2,23 +2,10 @@
   <div>
     <FilterMonthlyReport />
     <a-row :gutter="[16, 16]" :wrap="true">
-      <a-col
-        :xs="24"
-        :sm="24"
-        :md="24"
-        :lg="12"
-        :xl="8"
-        v-for="(item, index) in pieChartConfig"
-        :key="index"
-      >
-        <BasePieChart
-          :chart-title="item.title"
-          :unit="item.unit"
-          :series-data="
-            formatData(productionStore.monthlyProduction.data, item.key)
-          "
-          :total-data="productionStore.monthlyProduction.total?.[item.key]"
-        />
+      <a-col :xs="24" :sm="24" :md="24" :lg="12" :xl="8" v-for="(item, index) in pieChartConfig" :key="index">
+        <BasePieChart :chart-title="item.title" :unit="item.unit" :series-data="formatData(productionStore.monthlyProduction.data, item.key)
+          " :total-data="productionStore.monthlyProduction.total?.[item.key]">
+        </BasePieChart>
       </a-col>
     </a-row>
   </div>
@@ -49,7 +36,7 @@ const pieChartConfig = [
   { key: 'kadoritsu', title: 'Kadoritsu', unit: '%' },
   { key: 'reject_setting', title: 'Reject Setting', unit: 'Pin' },
   { key: 'dummy', title: 'Dummy', unit: 'Pin' },
-  { key: 'production_time', title: 'Production Time', unit: 'Pin' },
+  { key: 'production_time', title: 'Production Time', unit: 'Min' },
 ]
 
 const formatData = (data, key) => {
